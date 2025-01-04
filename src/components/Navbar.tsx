5930f13956a88194606c9f83ba2edacf87d2a1bf
Jagadeesh <jagadeesh28.m@gmail.com>
2025-01-04 21:18:38 +0530

    added dark and light theme

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50:">
+      NAVBAR
+    </nav>
+  );
+}
+
+export default Navbar;
