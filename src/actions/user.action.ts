fedeb191c8abd3ce73d082a12265382e83ff4573
Jagadeesh <jagadeesh28.m@gmail.com>
2025-01-19 17:47:23 +0530

    implemented sidebar

--- src/actions/user.action.ts
+++ src/actions/user.action.ts
@@ -33,3 +33,24 @@ export async function syncUser() {
     console.log("Error in syncUser", error);
   }
 }
+
+export async function getUserByClerkId(clerkId: string) {
+  try {
+    return await prisma.user.findUnique({
+      where: {
+        clerkId,
+      },
+      include: {
+        _count: {
+          select: {
+            followers: true,
+            following: true,
+            posts: true,
+          },
+        },
+      },
+    });
+  } catch (error) {
+    console.log("Error in syncUser", error);
+  }
+}
