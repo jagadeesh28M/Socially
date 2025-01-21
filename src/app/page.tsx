1bf2a283763f64e6a4e961de4e2238c2bb10a3a5
Jagadeesh <jagadeesh28.m@gmail.com>
2025-01-22 00:21:31 +0530

    creating posts and suggested users

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
-export default function Home() {
+import CreatePost from "@/components/CreatePost";
+import SuggestedUsers from "@/components/SuggestedUsers";
+import { currentUser } from "@clerk/nextjs/server";
+
+export default async function Home() {
+  const user = await currentUser();
   return (
-    <div className="m-4">
-      <h1>HOME PAGE</h1>
+    <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+      <div className="lg:col-span-6">{user ? <CreatePost /> : null}</div>
+      <div className="hidden lg:block lg:col-span-4 sticky top-20">
+        <SuggestedUsers />
+      </div>
     </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,9 +6,10 @@ import { syncUser } from "@/actions/user.action";
 
 async function Navbar() {
   const user = await currentUser();
-  if (user) await syncUser();
+  if (user) await syncUser(); // POST
+
   return (
-    <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50:">
+    <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
       <div className="max-w-7xl mx-auto px-4">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
@@ -27,5 +28,4 @@ async function Navbar() {
     </nav>
   );
 }
-
 export default Navbar;
